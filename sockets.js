e7a28e827be24a00eec5043ad3323ddaae72922b
Jose Andres <Gorusuke123@gmail.com>
2021-04-28 23:34:29 -0500

    Probando mandar mensajes con web sockets para el cliente

--- sockets.js
+++ sockets.js
@@ -2,109 +2,84 @@ const Messages = require("./model/Messages");
 // const { addUser, removeUser, getUser, users } = require("./helpers/users");
 
 module.exports = (io) => {
-  let users = {};
+  let users = [];
 
-  io.on("connection", async (socket) => {
-    // let mensajes = await Messages.find({}).limit(40).sort("-date");
+  io.on("connection", (socket) => {
+    // let mensajes = Messages.find({}).limit(10).sort("-date");
 
     // socket.emit("load old messages", mensajes);
 
-    // socket.on("connected", (username) => {
-    //   console.info("Usuario conectado", username);
-    //   socket.emit("message", {
-    //     user: "admin",
-    //     text: `@${username} welcome to this chat!`,
-    //   });
-
-    //   // socket.join(user.username);
-    // });
-
-    // socket.on("newUser", (data, callback) => {
-    //   socket.broadcast.emit("user", {
-    //     user: username,
-    //     text: `@${username}`,
-    //   });
-
-    //   callback({
-    //     users,
-    //   });
-    // });
-
     socket.on("newUser", (data, callback) => {
-      if (data in users) {
-        callback(false);
-      } else {
-        callback(true);
-        socket.username = data;
-        user[socket.username] = socket;
-        updateUsers();
-      }
-      console.info(users);
+      // if (data in users) {
+      //   callback(false);
+      // } else {
+      //   callback(true);
+      //   socket.username = data;
+      //   user[socket.username] = socket;
+      //   updateUsers();
+      // }
+      // console.info(users);
     });
 
-    // socket.on("sendMessage", (message) => {
-    //   const user = getUser(socket.id);
-    //   // console.info(user);
+    socket.on("connected", (username) => {
+      console.info("Usuario conectado", username);
+      socket.emit("message", {
+        user: "admin",
+        text: `@${username} welcome to this chat!`,
+      });
+      const user = {
+        id: socket.id,
+        username,
+      };
+      users.push(user);
+      console.info(users);
 
-    //   io.emit("message", { user: user.username, text: message });
-    // });
+      // socket.join(user.username);
+    });
+
+    socket.on("users", (callback) => {
+      callback({ users });
+    });
 
     socket.on("sendMessage", async (data, callback) => {
-      // let msg = data.trim();
-
-      // if (msg.substr(0, 3) === "/w ") {
-      //   msg = msg.substr(3);
-      //   let index = msg.indexOf(" ");
-      //   if (index !== -1) {
-      //     let name = msg.substring(0, index);
-      //     msg = substring(index + 1);
-      //     if (name in users) {
-      //       user[name].emit("whisper", {
-      //         msg,
-      //         user: socket.username,
-      //       });
-      //     } else {
-      //       callback("Error! Enter a valid User");
-      //     }
-      //   } else {
-      //     callback("Error! Please enter your message");
-      //   }
-      // } else {
-      let newMessage = new Messages({
+      const newMessage = new Messages({
         message: data.message,
         user: data.username,
         date: new Date().toISOString(),
       });
       await newMessage.save();
-
-      // io.emit("message", { user: data.username, message: data.message });
-      // }
-      console.info(data);
     });
 
-    // socket.on("disconnect", (callback) => {
-    //   const user = removeUser(socket.id);
-    //   if (user) {
-    //     console.info(user.username);
-    //     console.info(users);
-    //     io.emit("messages", {
-    //       user: "admin",
-    //       text: `@${user.username} Has left this chat`,
-    //     });
-    //     // callback({
-    //     //   users,
-    //     // });
-    //   }
+    socket.on("disconnect", (callback) => {
+      console.info(users);
+      const removeUser = (id) => {
+        const index = users.findIndex((user) => user.id === id);
+
+        if (index !== -1) {
+          return users.splice(index, 1)[0];
+        }
+      };
+      const user = removeUser(socket.id);
+      if (user) {
+        console.info("desde Disconect");
+        console.info(user.username);
+        console.info(users);
+        io.emit("message", {
+          user: "admin",
+          text: `@${user.username} Has left this chat`,
+        });
+        // callback({ users });
+      }
+    });
+
+    // socket.on("disconnect", (data) => {
+    //   if (!socket.username) return;
+    //   delete users[socket.username];
+    //   updateUsers();
     // });
 
-    socket.on("disconnect", (data) => {
-      if (!socket.username) return;
-      delete users[socket.username];
-      updateUsers();
-    });
-
-    function updateUsers(params) {
-      io.socket.emit("usernames", Object.keys(users));
-    }
+    // function updateUsers(params) {
+    //   io.socket.emit("usernames", Object.keys(users));
+    // }
   });
 };
